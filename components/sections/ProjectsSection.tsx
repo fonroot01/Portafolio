3452a5a28214a7a6442177a0d9613ba503990a18
fonroot01 <alfomsoque22@gmail.com>
2025-07-10 21:34:25 -0500

    ok

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -28,7 +28,7 @@ const ProjectsSection = () => {
     {
       id: 3,
       title: "GestorIP",
-      description: "Cambia tu dirección IP de forma rápida y sencilla, sin necesidad de usar la consola. Interfaz intuitiva para administradores de red y profesionales IT.",
+      description: "Cambia tu dirección IP de forma rápida y sencilla, sin necesidad de usar la consola. Permite gestionar múltiples conexiones de red y restablecer dhcp.",
       githubUrl: "https://github.com/fonroot01/GestorIP",
       tags: ["Python", "Tkinter", "Network Tools"],
       image: "/portadasp/gestorip.png",
